c7b156cc222e471d61d1fac80e0077ce7bc8a40a
Manqi Shi <Manqi@Manqis-Air-3.attlocal.net>
2020-04-03 00:35:18 -0500

    working on 2.5

---
+++ A2.5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int next_state(int state, string next_word, map table){
+     map<string, int> event = table[state];
+     if(event.count(next_word)){
+          return event[next_word];
+     }
+     else{
+          return -1;
+     }
+}
+
+int main(){
+     int state;
+     string next_word;
+     map<int, map<string, int>> table = {
+          {0, {"the", 1}},
+          {1, {"lazy", 2}},
+          {1, {"smelly", 2}}
+          //{"lazy", 2},
+     };
+
+     next_state(state, next_word, table)();   
+};
+
